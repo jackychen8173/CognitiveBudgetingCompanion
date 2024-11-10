29b37cad49f3344473177292f0953f66d61f536f
Jacky Chen <jchen8173@gmail.com>
2024-11-10 12:24:29 -0800

    fixed routing

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,48 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import Home from './pages/Home';   
-import Expenses from './pages/Expenses';
-import ExpensesReport from './pages/ExpensesReport';
-import SignIn from './pages/SignIn';
 
-// Define your routes
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "/Home",
-    element: <Home />,
-  },
-  {
-    path: "/Expenses",
-    element: <Expenses />,
-  },
-  {
-    path: "/ExpensesReport",
-    element: <ExpensesReport />,
-  },
-  {
-    path: "/SignIn",
-    element: <SignIn />,
-  },
-]);
-
-// Render the application with the router
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />  {/* Just render the App component */}
   </React.StrictMode>
 );
 
 // Optional: Measure app performance
 reportWebVitals();
-
-// https://www.geeksforgeeks.org/how-to-create-a-multi-page-website-using-react-js/
