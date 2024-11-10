1325ddf36816e598a045890d89c40f487ba6969a
Jacky Chen <jchen8173@gmail.com>
2024-11-10 10:45:45 -0800

    deleted default react stuff

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -6,22 +5,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <button className="button"> Sign In</button>
-        
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <p> You have spent $TODO money. </p>  
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
         <br></br>
-        <button> Settings </button>
+        <button className="button"> Settings </button>
       </header>
     </div>
   );
